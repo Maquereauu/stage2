85d40dbf973905aa3fc1da4b5c9a0419dd0c3d91
Maquereauu <macrorush@gmail.com>
2023-07-03 16:15:07 +0200

    update : test

--- back/index.js
+++ back/index.js
@@ -30,10 +30,6 @@ const allowCors = fn => async (req, res) => {
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
 }
 
 const handler = (req, res) => {
