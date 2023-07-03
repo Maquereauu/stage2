9da79b15d0ec4a0973912c7b8f3067615a82df99
Maquereauu <macrorush@gmail.com>
2023-07-03 14:25:56 +0200

    update : test

--- back/index.js
+++ back/index.js
@@ -4,6 +4,26 @@ const multer = require("multer");
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+allowCors(handler)
 var whitelist = ['ide-front.vercel.app','stage-dun.vercel.app']
 var corsOptions = {
   origin: function (origin, callback) {
