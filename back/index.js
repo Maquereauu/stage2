430053e93f0c68e18b0040c0a33fd5ae348c7d8f
Maquereauu <macrorush@gmail.com>
2023-07-04 20:25:09 +0200

    update : test

--- back/index.js
+++ back/index.js
@@ -23,6 +23,20 @@ const allowCorsHandler = (req, res, next) => {
 };
 app.use(allowCorsHandler);
 const port = 4444;
+const firebaseConfig = {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+};
+
+console.log(firebaseConfig)
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const { Sequelize } = require('sequelize');
@@ -142,7 +156,7 @@ app.post("/traitement/delete", jsonParser, (req, res) => {
 
 // CRUD photos
 const storage = new Storage({
-  keyFilename: 'images-3e2d3-firebase-adminsdk-c8g82-513c6802ee.json',
+  keyFilename: firebaseConfig,
   projectId: 'images-3e2d3',
 });
 const bucket = storage.bucket('images-3e2d3.appspot.com');
