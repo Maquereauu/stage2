a2a52634e152793c932226617326d4788bae3709
Maquereauu <macrorush@gmail.com>
2023-07-05 14:38:07 +0200

    update : test

--- back/index.js
+++ back/index.js
@@ -4,18 +4,22 @@ const { Storage } = require('@google-cloud/storage');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
-const allowCorsHandler = (req, res, next) => {
+const allowCorsHandler = async (req, res, next) => {
   const whitelist = ['https://ide-front.vercel.app', 'https://stage-dun.vercel.app','https://vercel.com','http://localhost:3000'];
   const origin = req.headers.origin;
   if (whitelist.indexOf(origin) !== -1) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', origin)
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.sendStatus(200);
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
   } else {
     return res.status(403).json({ error: 'Non'});
   }
