6298d3ccfc0a0a9a9fd6353d8ad51fcc8494b5b9
Maquereauu <macrorush@gmail.com>
2023-07-06 20:51:35 +0200

    update : test

--- back/index.js
+++ back/index.js
@@ -11,6 +11,7 @@ const allowCorsHandler = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Cache-Control', 'no-cache');
     res.setHeader(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
