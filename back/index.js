e878adddaeb971339ee3266a737eb37def5b0c7b
Maquereauu <macrorush@gmail.com>
2023-07-07 16:15:15 +0200

    update : final + new database

--- back/index.js
+++ back/index.js
@@ -54,7 +54,7 @@ const firebaseConfig = {
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('sql7630695', 'sql7630695', process.env.PASSWORD, {
+const sequelize = new Sequelize('bbhdprtg0bomy642ezho', 'up9qasebeb3nnzfa', process.env.PASSWORD, {
   host: process.env.LINK,
   dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
   dialectModule: require('mysql2'),
