50eea5106b603c054e7e7cf6684d18fa4125603b
Maquereauu <macrorush@gmail.com>
2023-07-12 17:56:47 +0200

    update : test

--- back/index.js
+++ back/index.js
@@ -60,9 +60,9 @@ const sequelize = new Sequelize('basetest2', 'maquereau', process.env.PASSWORD, 
   dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
   dialectModule: require('mysql2'),
   pool: {
-    max: 10,
+    max: 8,
     min: 0, 
-    acquire: 15000, 
+    acquire: 10000, 
     idle: 1500
   }
 });
